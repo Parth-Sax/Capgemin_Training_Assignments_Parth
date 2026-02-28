6edb0121af82e702d9d7adcd6f240c02858fa224
parthsaxena <151524776+Parth-Sax@users.noreply.github.com>
2026-02-28 10:43:39 +0530

    Create Employee.java

---
+++ Advance_java/JDBC/Callable_Statements/callable_and_storedpro_and_properties/Employee.java
@@ -0,0 +1,15 @@
+package callable_and_storedpro_and_properties;
+import jdk.jfr.DataAmount;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class Employee {
+    private int id;
+    private String name;
+    private double salary;
+}
