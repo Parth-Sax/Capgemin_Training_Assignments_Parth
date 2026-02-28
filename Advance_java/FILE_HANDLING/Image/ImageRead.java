958a509e02bef10d972fcabae5fca45c3800ecb9
parthsaxena <151524776+Parth-Sax@users.noreply.github.com>
2026-02-28 09:46:04 +0530

    Create ImageRead.java

---
+++ Advance_java/FILE_HANDLING/Image/ImageRead.java
@@ -0,0 +1,18 @@
+package Image;
+import java.io.*;
+
+public class ImageRead {
+    public static void main(String[] args) {
+        String path="Image/copied.png";
+        try(FileInputStream fs= new FileInputStream(path)){
+            int size=0;
+            while (fs.read()!=-1){
+                size++;
+            }
+            System.out.println("Image read succesfullly");
+            System.out.println("Image Size: "+size+" bytes");
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+}
