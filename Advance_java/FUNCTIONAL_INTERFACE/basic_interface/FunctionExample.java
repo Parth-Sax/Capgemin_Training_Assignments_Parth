5402814af0d6687c5a07ca803851a8ce3f62f3e1
parthsaxena <151524776+Parth-Sax@users.noreply.github.com>
2026-02-28 09:53:34 +0530

    Create FunctionExample.java

---
+++ Advance_java/FUNCTIONAL_INTERFACE/basic_interface/FunctionExample.java
@@ -0,0 +1,16 @@
+package basic_interface;
+import java.util.function.Function;
+public class FunctionExample {
+    public static void main(String[] args) {
+        Function<Integer, Integer> function = new Function<>() {
+            @Override
+            public Integer apply(Integer integer) {
+                return integer * integer;
+            }
+        };
+        System.out.println(function.apply(12));
+        // -----------------------------------------------
+        Function<Integer, Integer> function1 = number -> number * number;
+        System.out.println(function1.apply(11));
+    }
+}
