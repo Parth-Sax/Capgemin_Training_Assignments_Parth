c7fa054a107973880143853959fdd3a71ea3cb9e
parthsaxena <151524776+Parth-Sax@users.noreply.github.com>
2026-02-28 10:28:18 +0530

    Create AutoBoxing.java

---
+++ Core_Java/AutoBoxing.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Autoboxing {
+    public static void main(String[] args) {
+        int a = 10;
+        Integer b = 11;
+        b = a; // Autoboxing
+        a = b; // AutoUnboxing
+    }
+}
